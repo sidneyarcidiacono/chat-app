cb8674a9ac1519095d7b44d212afcffb23e4e3a0
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-04-22 21:31:22 -0700

    [update] Including socket.io code to emit messages and open connection.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const path = require('path')
-
+const http = require('http');
 const app = express()
+const server = http.createServer(app);
+
+const { Server } = require('socket.io')
+const io = new Server(server)
 
 const mainRoutes = require('./routes/main')
 
@@ -11,6 +15,12 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(mainRoutes)
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg)
+  })
+})
+
+server.listen(3000, () => {
   console.log('CONNECTED')
 })
